2f006b7ae9b2dc2bedd58ac260e85150c828954d
IdoyaGE <125289400+IdoyaGE@users.noreply.github.com>
2023-02-23 11:34:41 +0100

    Add files via upload

---
+++ pruebaidoya.js
@@ -0,0 +1,50 @@
+/*Ejercicio 1: Contar con los dedos*/
+let arraycontar = [];
+for (let i = 1; i<=10; i++) {
+    arraycontar.push(i); 
+}
+console.log(arraycontar);
+
+/*Ejercicio 2: Sumar bote*/
+
+let suma = [i]; 
+for (let i = 0; i < array.length; i++) { 
+    suma += array[i]; 
+} 
+    console.log(suma);
+
+/*Ejercicio 3: Devolver edades >18*/
+
+let Edad = [17,18,20];
+
+for (let i = 0; i < Edad.lenght; i++){
+   if (Edad >= 18){
+    Edad.push (i); 
+}
+}
+console.log (Edad);
+
+/*Ejercicio 4: buscar palabras
+Ejemplo: buscarPalabra("hola", ["hola", "adiós"]) devolverá true
+Ejemplo: buscarPalabra("hola", ["adiós", "buenas"]) devolverá false
+
+Split de las palabras y posiciones con asteriscos***/
+
+let a = ["hola", "adios", "buenas"]
+
+function buscar(a) {
+    for (let i = 0; i < array.length; i++)
+    {
+        if (a ===  ) {
+            return false;
+        }  
+        if (a === ){
+            return true;
+        }         
+        
+    }
+buscarPalabra()
+
+
+
+
